6cb6cda20e30845d07e33c52f6a63a4873037606
srvnnithya <srvn.nithya@gmail.com>
2023-10-24 20:28:59 +0530

    Updated Hosts

--- Connect/settings.py
+++ Connect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gt^6bo^6(c75w*0cj$guj09^9m%z=1dsu&7pfn-n2lm!xk$1x-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['srvnnithya.pythonanywhere.com']
 
 
 # Application definition
